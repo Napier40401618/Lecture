3aac450970520bb5c0ca78f79078d60e25a38eac
Napier40401618 <zayyanpaing95@gmail.com>
2018-04-25 10:37:54 +0630

    Bootstrap

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+from  flask  import  Flask , render_template
+app = Flask(__name__)
 
-@app.route('/')
-def default():
-	return("Hello!")
+@app.route('/inherits/')
+def  inherits ():
+        return  render_template('base.html')
 
-@app.route('/hello/<name>')
-def hello(name=None):
-	user={'name':name}
-	return render_template('hello.html',user=user)
+@app.route('/inherits/one/')
+def  inherits_one ():
+        return  render_template('in1.html')
 
-if __name__=="__main__":
-	app.run(debug=True)
+@app.route('/inherits/two/')
+def  inherits_two ():
+        return  render_template('in2.html')
+
+if  __name__  == ("__main__"):
+        app.run(host='0.0.0.0', debug=True)
